a8daf24c4e345d8ca2e866f059a48aab08e17fd8
TourBandLive <tourbandlive@gmail.com>
2025-01-10 10:47:05 +0100

    fin seccion 10

---
+++ src/common/dtos/pagination.dto.ts
@@ -0,0 +1,16 @@
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
+
+export class PaginationDto {
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  @Type(() => Number)
+  limit?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Type(() => Number)
+  offset?: number;
+}
---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,46 @@
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @Min(0)
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  tags?: string[];
+}
